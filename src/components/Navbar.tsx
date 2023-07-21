0ab9b95e4e1113ed1e96903c89d9823e50a25bf8
matiaslascano <matias_lascano@hotmail.com>
2023-07-21 13:35:08 -0300

    Subiendo la primer parte: Header y Slider listos.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
+
+const user = false;
+
+const Navbar = () => {
+  return (
+    <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 md:h-24 lg:px-20 xl:px-40">
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">HOME</Link>
+        <Link href="/menu">MENU</Link>
+        <Link href="/">CONTACT</Link>
+      </div>
+      <div className="uppercase text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Trattoria</Link>
+      </div>
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="flex items-center cursor-pointer bg-red-300 px-1 rounded-md md:absolute top-3 right-2 lg:static">
+          <Image src="/phone.png" alt="telefono" width={20} height={20} />
+          <span className="text-white">+5493514084026</span>
+        </div>
+        {!user ? (
+          <Link href="/login">LOGIN</Link>
+        ) : (
+          <Link href="/orders">ORDERS</Link>
+        )}
+        <CartIcon />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Slider.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "Siempre fresco, crujiente y caliente",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "Entregamos su pedido en cualquier lugar de NY",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: "La mejor pizza para compartir con tu familia",
+    image: "/slide3.jpg",
+  },
+];
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev == data.length - 1 ? 0 : prev + 1));
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] md:flex-row ">
+      <div className="flex flex-1 items-center justify-center flex-col gap-8 text-red-500 font-bold ">
+        <h1 className="text-5xl text-center uppercase font-bold md:text-6xl lg:text-7xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-500 text-white py-4 px-8 ">
+          Ordene ahora
+        </button>
+      </div>
+      <div className="w-full flex-1 relative">
+        <Image
+          src={data[currentSlide].image}
+          alt="imagen-slider-1"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
